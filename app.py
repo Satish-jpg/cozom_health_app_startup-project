562ce0bf7ee0b65ba9ededdf31acb24c44db51c3
Satish-jpg <70453282+Satish-jpg@users.noreply.github.com>
2024-10-09 11:41:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def home():
     return "Hello, Vercel!"
 
+@app.route('/about')
+def about():
+    return "This is the About page."
+
+@app.route('/contact')
+def contact():
+    return "Contact us at contact@cozom.in"
+
 if __name__ == "__main__":
     app.run()
